0af80c8b5b5bd26e8af07c520e129b9f1531fab5
eladamit555 <eladamit555@gmail.com>
2026-07-16 21:13:25 +0300

    .

--- while_true3.py
+++ while_true3.py
@@ -5,7 +5,6 @@ while True:
     user = input('username: ')
     password = input('password: ')
     if user == 'admin' and password == '1234':
-
         print('logged in')
         break
     else:
